9bee39e1f6d9164f3ee50242c596a0aee737cac0
unknown <davismeru911@gmail.com>
2023-08-20 21:20:12 +0300

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
-import { FaTv} from 'react-icons/fa6'
-import { BiSearch, BiTv } from 'react-icons/bi'
+import { BiSearch } from 'react-icons/bi'
 import { useState } from 'react'
 import { fetchFromApi } from '../assets/fetchFromApi'
 import { useNavigate } from 'react-router-dom'
@@ -20,23 +19,29 @@ function Navbar() {
     })
   }
   return (
-    <div className='nav p-5 flex flex-col gap-2 justify-between items-center nav rounded-sm mb-3 md:flex-row'>
+    <div className='nav p-5 flex flex-col gap-2 justify-between items-center nav rounded-sm mb-3 h-[60vh] md:flex-row relative overflow-hidden'>
+      {/* bg image */}
+      <img src="banner.jpg" className='absolute w-full h-full left-0 top-0 object-cover' />
+
+      {/* dark overlay */}
+      <section className='absolute bg-black w-full h-full left-0 top-0 opacity-50'></section>
+
         {/* logo */}
         <Link to={`/`}>
-          <div className='flex items-center text-3xl font-bold gap-2 text-red-500 md:text-3xl'>
-                <BiTv className='text-5xl'/>
-                <p>Y<span className='text-sm text-gray-500'>Flix</span></p>
+          <div className='flex flex-col text-5xl text-center font-bold gap-2 text-red-600 md:text-8xl'>
+                <p className='z-20'>Y<span className='text-2xl md:text-6xl text-white'>Flix</span></p>
+                <p className='z-20 text-sm text-gray-300'>Stream and download your favourite videos</p>
           </div>
         </Link>
 
         {/* Search */}
-        <form className='flex items-center relative text-red-500  ' onSubmit={(e) => handleSubmit(e)}>
+        <form className='flex relative text-red-500 md:self-start' onSubmit={(e) => handleSubmit(e)}>
             <input type="text"
-              className='pl-6 text-sm outline-none border-[1px] text-gray-800 rounded-md  py-1 md:py-2 md:w-60'
+              className='pl-8 text-sm outline-none border-[1px] border-gray-600 text-white rounded-sm  py-1 bg-transparent md:py-2 md:w-60'
               value={value}
               onChange={(e) => setValue(e.target.value)}
             />
-            <button className='absolute left-1'><BiSearch/></button>
+            <button className='absolute top-1/2 left-2 -translate-y-1/2 text-gray-200'><BiSearch/></button>
         </form>
     </div>
   )
